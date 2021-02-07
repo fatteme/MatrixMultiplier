7179a324e1c14046d258a7c01a51eaf221177bcf
ArshiA Akhavan <letmemakenewone@gmail.com>
2021-02-07 08:17:42 +0330

    test bench bug fix for adder

--- single_adder_TB.v
+++ single_adder_TB.v
@@ -31,10 +31,10 @@ module adder_TB();
             a, b, z);
     
     //{s, e, m}
-    one           = { 1'b0 , 1'b0,{7{1'b1}} , {{21{1'b0}},2'b01} };
-    two           = { 1'b0 , 1'b0,{7{1'b1}} , {{21{1'b0}},2'b10} };
-    neg_one       = { 1'b1 , 1'b0,{7{1'b1}} , {{22{1'b0}},2'b01} };
-    neg_two       = { 1'b1 , 1'b0,{7{1'b1}} , {{21{1'b0}},2'b10} };
+    one           = 32'b00111111100000000000000000000000;
+    two           = 32'b01000000000000000000000000000000;
+    neg_one       = 32'b10111111100000000000000000000000;
+    neg_two       = 32'b11000000000000000000000000000000;
     pos_infinity  = { 1'b0 , {8{1'b1}} , {23{1'b0}} };
     neg_infinity  = { 1'b1 , {8{1'b1}} , {21{1'b0}} };
     nan           = { 1'b1 , {8{1'b1}} , {{22{1'b0}},1'b1} };
@@ -44,19 +44,19 @@ module adder_TB();
     rst = 1;
     #1000
    
-    // // 1 + 1 (two positive numbers)
-    // $display("test1: 1 + 1");
-    // rst = 0;
-    // a = one;
-    // b = one;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // 1 + 1 (two positive numbers)
+    $display("test0: 1 + 1");
+    rst = 0;
+    a = one;
+    b = one;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
     
-    // 1 + (-2) (one positive and one negative number)
+    // 1 + (2) (two positive number)
     rst = 1;
     #1000
-    $display("test2: 1 + 2");
+    $display("test1: 1 + 2");
     rst = 0;
     a_stb = 0;
     b_stb = 0;
@@ -66,149 +66,149 @@ module adder_TB();
     b_stb = 1;
     #1000;
 
-    // // 1 + (-2) (one positive and one negative number)
-    // rst = 1;
-    // #1000
-    // $display("test2: 1 + (-2)");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = one;
-    // b = neg_two;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // 1 + (-2) (one positive and one negative number)
+    rst = 1;
+    #1000
+    $display("test2: 1 + (-2)");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = one;
+    b = neg_two;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
     
-    // // -1 + 2 (one negative and one positive number)
-    // rst = 1;
-    // #1000
-    // $display("test3: -1 + 2");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = neg_one;
-    // b = two;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // -1 + 2 (one negative and one positive number)
+    rst = 1;
+    #1000
+    $display("test3: -1 + 2");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = neg_one;
+    b = two;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
-    // // -1 + (-2) (two negative numbers)
-    // rst = 1;
-    // #1000
-    // $display("test4: -1 + (-2)");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = neg_one;
-    // b = neg_two;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // -1 + (-2) (two negative numbers)
+    rst = 1;
+    #1000
+    $display("test4: -1 + (-2)");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = neg_one;
+    b = neg_two;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
-    // // 1 + nan (number and not a number)
-    // rst = 1;
-    // #1000
-    // $display("test5: 1 + NAN");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = one;
-    // b = nan;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // 1 + nan (number and not a number)
+    rst = 1;
+    #1000
+    $display("test5: 1 + NAN");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = one;
+    b = nan;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
-    // // pos_infinity + nan (infinity and not a number)
-    // rst = 1;
-    // #1000
-    // $display("test6: infinity + NAN");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = pos_infinity;
-    // b = nan;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // pos_infinity + nan (infinity and not a number)
+    rst = 1;
+    #1000
+    $display("test6: infinity + NAN");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = pos_infinity;
+    b = nan;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
-    // // neg_infinity + nan (infinity and not a number)
-    // rst = 1;
-    // #1000
-    // $display("test7: -infinity + NAN");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = neg_infinity;
-    // b = nan;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // neg_infinity + nan (infinity and not a number)
+    rst = 1;
+    #1000
+    $display("test7: -infinity + NAN");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = neg_infinity;
+    b = nan;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
-    // // pos_infinity + number (infinity and a number)
-    // rst = 1;
-    // #1000
-    // $display("test8: infinity + 1");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = pos_infinity;
-    // b = one;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // pos_infinity + number (infinity and a number)
+    rst = 1;
+    #1000
+    $display("test8: infinity + 1");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = pos_infinity;
+    b = one;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
-    // // neg_infinity + number (infinity and a number)
-    // rst = 1;
-    // #1000
-    // $display("test9: -infinity + 1");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = neg_infinity;
-    // b = one;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // neg_infinity + number (infinity and a number)
+    rst = 1;
+    #1000
+    $display("test9: -infinity + 1");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = neg_infinity;
+    b = one;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
 
-    // // pos_infinity + pos_infinity 
-    // rst = 1;
-    // #1000
-    // $display("test10: infinity + infinity");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = pos_infinity;
-    // b = pos_infinity;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // pos_infinity + pos_infinity 
+    rst = 1;
+    #1000
+    $display("test10: infinity + infinity");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = pos_infinity;
+    b = pos_infinity;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
-    // // neg_infinity + neg_infinity 
-    // rst = 1;
-    // #1000
-    // $display("test11: -infinity + (-infinity)");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = neg_infinity;
-    // b = neg_infinity;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // neg_infinity + neg_infinity 
+    rst = 1;
+    #1000
+    $display("test11: -infinity + (-infinity)");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = neg_infinity;
+    b = neg_infinity;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
-    // // pos_infinity + neg_infinity (pos_infinity + neg_infinity must be nan)
-    // rst = 1;
-    // #1000
-    // $display("test12: infinity + (-infinity)");
-    // rst = 0;
-    // a_stb = 0;
-    // b_stb = 0;
-    // a = pos_infinity;
-    // b = neg_infinity;
-    // a_stb = 1;
-    // b_stb = 1;
-    // #1000;
+    // pos_infinity + neg_infinity (pos_infinity + neg_infinity must be nan)
+    rst = 1;
+    #1000
+    $display("test12: infinity + (-infinity)");
+    rst = 0;
+    a_stb = 0;
+    b_stb = 0;
+    a = pos_infinity;
+    b = neg_infinity;
+    a_stb = 1;
+    b_stb = 1;
+    #1000;
 
     $stop;  
     $finish;
