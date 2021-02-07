71fb7f5911ca930cfe3546fad249d1372af9a1ce
ArshiA Akhavan <letmemakenewone@gmail.com>
2021-02-07 06:51:06 +0330

    same shits for fp_mult

--- inner_product.v
+++ inner_product.v
@@ -40,18 +40,19 @@ module inner_product #(parameter number_of_elements = 4)(
                 state_wait_for_add  = 3'd4,
                 state_out_is_ready  = 3'd5;
 
-    wire [word_width-1:0] vector_mult_result [1:number_of_elements];
     reg [2:0] state;
     reg [word_width-1:0] inner_product_result = 32'b0;
+    
     // multiplier signals
-    reg rst_mult = 0;
-    reg in1_stb = 0;
-    reg column_stb = 0;
-    reg output_ack = 0;
-    wire output_stb [1:number_of_elements];
-    wire in1_ack_mult[1:number_of_elements];
-    wire column_ack_mult[1:number_of_elements];
-    reg output_full_stb = 1;
+    wire [word_width-1:0] mul_out [1:number_of_elements];
+    wire mul_out_stb [1:number_of_elements];
+    wire mul_in1_ack [1:number_of_elements];
+    wire mul_in2_ack [1:number_of_elements];
+    reg mul_rst = 0;
+    reg mul_in1_stb = 0;
+    reg mul_in2_stb = 0;
+    reg mul_out_ack = 0;
+    reg mul_out_full_stb = 1;
     reg res_ack = 0;
 
     // adder signals
@@ -72,15 +73,15 @@ module inner_product #(parameter number_of_elements = 4)(
                 multiplier fp_mult(
                     .input_a(row[32 * i - 1 : 32 * (i-1)]),
                     .input_b(column[32 * i - 1 : 32 * (i-1)]),
-                    .input_a_stb(in1_stb),
-                    .input_b_stb(column_stb),
-                    .output_z_ack(output_ack),
+                    .input_a_stb(mul_in1_stb),
+                    .input_b_stb(mul_in2_stb),
+                    .output_z_ack(mul_out_ack),
                     .clk(clk),
-                    .rst(rst_mult),
-                    .output_z(vector_mult_result[i]),
-                    .output_z_stb(output_stb[i]),
-                    .input_a_ack(in1_ack_mult[i]),
-                    .input_b_ack(column_ack_mult[i])
+                    .rst(mul_rst),
+                    .output_z(mul_out[i]),
+                    .output_z_stb(mul_out_stb[i]),
+                    .input_a_ack(mul_in1_ack[i]),
+                    .input_b_ack(mul_in2_ack[i])
                 );
 
                 
@@ -89,16 +90,16 @@ module inner_product #(parameter number_of_elements = 4)(
     integer g =0;
     always @* begin
         g = 1; 
-        output_full_stb = 1;
+        mul_out_full_stb = 1;
             for(g=1;g<=number_of_elements;g=g+1)begin
-                output_full_stb = output_full_stb & output_stb[g];
+                mul_out_full_stb = mul_out_full_stb & mul_out_stb[g];
             end
     end
 
 
     integer index = 1;
     adder fp_add(
-        .input_a(vector_mult_result[index]),
+        .input_a(mul_out[index]),
         .input_b(inner_product_result),
         .input_a_stb(adder_in1_stb),
         .input_b_stb(adder_in2_stb),
@@ -115,7 +116,7 @@ module inner_product #(parameter number_of_elements = 4)(
         if(!rst)
         begin
             state <= state_idle;
-            rst_mult <= 1;
+            mul_rst <= 1;
             adder_rst <= 1;
             // ##
             s_row_i_ack <= 0;
@@ -127,20 +128,20 @@ module inner_product #(parameter number_of_elements = 4)(
                 state_idle: begin
                         if (row_i_stb & column_i_stb & out_o_ack) begin
                             state <= state_mult_elements;
-                            in1_stb <= 1;
-                            column_stb <= 1;
-                            rst_mult <= 0;
+                            mul_in1_stb <= 1;
+                            mul_in2_stb <= 1;
+                            mul_rst <= 0;
                             // ##
                             s_row_i_ack <= 1;
                             s_column_i_ack <= 1;
                         end
                         else begin
                             state <= state_idle;
-                            rst_mult <= 1;
-                            output_ack <= 0;
+                            mul_rst <= 1;
+                            mul_out_ack <= 0;
                             res_ack <= 0;
-                            in1_stb <= 0;
-                            column_stb <= 0;
+                            mul_in1_stb <= 0;
+                            mul_in2_stb <= 0;
                             // ##
                             s_row_i_ack <= 0;
                             s_column_i_ack <= 0;
@@ -148,16 +149,16 @@ module inner_product #(parameter number_of_elements = 4)(
                         end
                     end
                 state_mult_elements: begin
-                        in1_stb <= 1;
-                        column_stb <= 1;
+                        mul_in1_stb <= 1;
+                        mul_in2_stb <= 1;
 
-                        output_ack <= 0;
+                        mul_out_ack <= 0;
                         state <= state_wait_for_mult;
                     end
                 state_wait_for_mult: begin
-                    if(output_full_stb) begin
+                    if(mul_out_full_stb) begin
                             state <= state_add_elements;
-                            rst_mult <= 1;
+                            mul_rst <= 1;
                             adder_rst <= 0;
                             inner_product_result <= 0;
                             adder_in1_stb <= 1;
@@ -195,10 +196,10 @@ module inner_product #(parameter number_of_elements = 4)(
                 end
                 state_out_is_ready: begin
                     state <= state_idle;
-                    output_ack <= 0;
+                    mul_out_ack <= 0;
                     res_ack <= 0;
-                    in1_stb <= 0;
-                    column_stb <= 0;
+                    mul_in1_stb <= 0;
+                    mul_in2_stb <= 0;
                     // ##
                     s_out_o_stb <= 1;
                 end
